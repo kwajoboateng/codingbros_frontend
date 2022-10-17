ace1664fb84123110cb943a8f3b7e93ecf54fba2
kwajo <kwajoboateng31@gmail.com>
2022-10-17 13:12:03 -0400

    prep work

---
+++ src/common/home.js
@@ -0,0 +1,12 @@
+import Footer from "./footer/footer";
+import NavBar from "./navbar/navbar";
+
+
+export default function Home(){
+    return(
+        <>
+            <NavBar/>
+            <Footer/>
+        </>
+    )
+}
---
+++ src/common/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar(){
+    return(
+        <>
+            <div>
+                <h1> Header </h1>
+            </div>
+        </>
+    )
+}
